19f39f6dedd837da377dbdb95d7e93dc33e8c577
ETEOT-FCOT <111497716+ETEOT-FCOT@users.noreply.github.com>
2022-08-23 13:30:34 -0300

    Atualização
    Adicionada mais uma pergunta ao quiz

--- js/script.js
+++ js/script.js
@@ -131,3 +131,46 @@ function resposta3() {
 
     return false;
 }
+function resposta4() {
+    let resp = document.getElementsByName("resp4");
+    let op1 = document.getElementById("opcao1.4");
+    let op2 = document.getElementById("opcao2.4");
+    let op3 = document.getElementById("opcao3.4");
+    let op4 = document.getElementById("opcao4.4");
+    if (resp[3].checked == true) {
+        op1.style.color = "#9d0208"
+        op2.style.color = "#9d0208"
+        op3.style.color = "#9d0208"
+        op4.style.backgroundColor = "#80c423"
+        op4.style.color = "white"
+    }
+    else {
+        if (resp[0].checked == true) {
+            op1.style.backgroundColor = "#9d0208"
+            op1.style.color = "white"
+            op2.style.color = "#9d0208"
+            op3.style.color = "#9d0208"
+            op4.style.color = "#80c423"
+        }
+        else if (resp[1].checked == true) {
+            op1.style.color = "#9d0208"
+            op2.style.backgroundColor = "#9d0208"
+            op2.style.color = "white"
+            op3.style.color = "#9d0208"
+            op4.style.color = "#80c423"
+        }
+        else if (resp[2].checked == true) {
+            op1.style.color = "#9d0208"
+            op2.style.color = "#9d0208"
+            op3.style.backgroundColor = "#9d0208"
+            op3.style.color = "white"
+            op4.style.color = "#80c423"
+        }
+        
+    }
+
+    document.getElementById("bconf4").style.display = "none";
+    document.getElementById("bpro4").style.display = "block"
+
+    return false;
+}
